6b86e28fda672cc525c1d982965adced01e59c10
Fabien Marteau <fabien.marteau@armadeus.com>
2021-09-17 16:18:09 +0200

    try to display rainbow

--- src/main/scala/PatternExample.scala
+++ src/main/scala/PatternExample.scala
@@ -21,15 +21,62 @@ class PatternExample extends Module {
 
   val hv_sync = Module(new HVSync(vp)) // Synchronize VGA module
   val video_de = hv_sync.io.display_on
-  /* hdmi transmission */
 
+  /* generate rainbow */
+  /* inspired from http://blog.vermot.net/2011/11/03/generer-un-degrade-en-arc-en-ciel-en-fonction-d-une-valeur-programmatio/ */
+  val cTrig1 = 255.U
+  val cTrig2 = 510.U
+  val cTrig3 = 765.U
+  val cTrig4 = 1020.U
+  val cTrig5 = 1275.U
+  val cTrig6 = 1530.U
+  val hpos = hv_sync.io.hpos
+  val pred = Wire(UInt(8.W))
+  when(hpos < cTrig1){
+    pred := cTrig1
+  }.elsewhen(hpos < cTrig2) {
+    pred := cTrig2 - hpos
+  }.elsewhen(hpos < cTrig4){
+    pred := 0.U
+  }.elsewhen(hpos < cTrig5){
+    pred := hpos - cTrig4
+  }.otherwise{
+    pred := cTrig1
+  }
+  val pgreen = Wire(UInt(8.W))
+  when(hpos < cTrig1){
+    pgreen := hpos 
+  }.elsewhen(hpos < cTrig3){
+    pgreen := cTrig1
+  }.elsewhen(hpos < cTrig4){
+    pgreen := cTrig4 - hpos
+  }.otherwise{
+    pgreen := 0.U
+  }
+
+  val pblue  = Wire(UInt(8.W))
+  when(hpos < cTrig2){
+    pblue := 0.U
+  }.elsewhen(hpos < cTrig3){
+    pblue := hpos - cTrig2
+  }.elsewhen(hpos < cTrig5){
+    pblue := cTrig1
+  }.elsewhen(hpos < cTrig6){
+    pblue := cTrig6 - hpos
+  }.otherwise {
+    pblue := 0.U
+  }
+  
+
+
+  /* hdmi transmission */
   val rgb2tmds = Module(new Rgb2Tmds())
   rgb2tmds.io.videoSig.de := video_de 
   rgb2tmds.io.videoSig.hsync := hv_sync.io.hsync
   rgb2tmds.io.videoSig.vsync := hv_sync.io.vsync
-  rgb2tmds.io.videoSig.pixel.red   := Mux(video_de, "h00".U(8.W), 0.U)
-  rgb2tmds.io.videoSig.pixel.green := Mux(video_de, "h00".U(8.W), 0.U)
-  rgb2tmds.io.videoSig.pixel.blue  := Mux(video_de, "hFF".U(8.W), 0.U)
+  rgb2tmds.io.videoSig.pixel.red   := Mux(video_de, pred, 0.U)
+  rgb2tmds.io.videoSig.pixel.green := Mux(video_de, pgreen, 0.U)
+  rgb2tmds.io.videoSig.pixel.blue  := Mux(video_de, pblue, 0.U)
 
   
 
